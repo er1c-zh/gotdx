1de9f2da7e9b9a967fc626504d25f03c58be9efb
eric <ericzhao96@hotmail.com>
2024-10-31 13:27:00 +0800

    feat: add command panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Connect, FetchStatus } from "../wailsjs/go/api/App";
 import { api } from "../wailsjs/go/models";
 import { EventsEmit, EventsOn } from "../wailsjs/runtime";
+import CommandPanel from "./components/CommandPanel";
 
 function App() {
   const [msg, setMsg] = useState("better tdx");
@@ -20,43 +21,63 @@ function App() {
   };
 
   function connect() {
+    Connect("").then((msg) => {
+      setServerMsg(msg);
+    });
+  }
+
+  useEffect(() => {
     EventsOn(api.MsgKey.processMsg, (msg: api.ProcessInfo) => {
       updateMsg(msg.Msg);
     });
     EventsOn(api.MsgKey.connectionStatus, (connectionStatus: number) => {
       setIsConnected(connectionStatus > 0);
     });
-    Connect("").then((msg) => {
-      setServerMsg(msg);
-    });
-  }
+  }, []);
 
   function fetchStatus() {
     FetchStatus().then(updateIndexInfo);
   }
 
+  // command panel
+  const [cmdPanelShow, setCmdPanelShow] = useState(false);
+
   return (
     <div id="App" className="container bg-gray-900 h-screen">
-      <div id="status-bar" className="flex mb-2 bg-gray-700 space-x-2">
-        <div className={`px-2 w-1/5 ${isConnected ? "bg-green-900" : "bg-red-900"}`}>
-          <span>{isConnected ? "connected" : "disconnected"}</span>
+      <div id="content" className={`${cmdPanelShow ? "blur-sm" : ""}`}>
+        <div id="status-bar" className="flex mb-2 bg-gray-700 space-x-2">
+          <div
+            className={`px-2 w-1/5 ${
+              isConnected ? "bg-green-900" : "bg-red-900"
+            }`}
+          >
+            <span>{isConnected ? "connected" : "disconnected"}</span>
+          </div>
+          <div className="w-1/5">
+            <span>{serverMsg}</span>
+          </div>
+          <div className="w-3/5 text-left">
+            <span>{msg}</span>
+          </div>
         </div>
-        <div className="w-1/5">
-          <span>{serverMsg}</span>
-        </div>
-        <div className="w-3/5 text-left">
-          <span>{msg}</span>
+        <div>
+          <div className="space-x-4">
+            <button className="action-btn" onClick={connect}>
+              Connect
+            </button>
+            <button className="action-btn" onClick={fetchStatus}>
+              FetchStatus
+            </button>
+          </div>
         </div>
       </div>
-      <div>
-        <div className="space-x-4">
-          <button className="action-btn" onClick={connect}>
-            Connect
-          </button>
-          <button className="action-btn" onClick={fetchStatus}>
-            FetchStatus
-          </button>
-        </div>
+      <div
+        id="command-panel"
+        className={`${
+          cmdPanelShow ? "" : "hidden"
+        } w-screen fixed top-0 left-0`}
+      >
+        <CommandPanel setIsShow={setCmdPanelShow} />
       </div>
     </div>
   );
--- frontend/src/components/CommandPanel.tsx
+++ frontend/src/components/CommandPanel.tsx
@@ -1,4 +1,49 @@
-import "../App.css"
-function CommandPanel() {
-    return <div>CommandPanel</div>;
+import "../App.css";
+import { useEffect, useRef, useState } from "react";
+
+interface CommandPanelProps {
+  setIsShow: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+function CommandPanel(props: CommandPanelProps) {
+  const [cmd, setCmd] = useState("");
+  let isShow = false;
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+    document.addEventListener("keyup", (e) => {
+      if (isShow && e.key === "Escape") {
+        isShow = false;
+        props.setIsShow(false);
+        setCmd("");
+      } else if (!isShow && /^[0-9a-zA-Z]+$/.test(e.key)) {
+        isShow = true;
+        props.setIsShow(true);
+      }
+    });
+  });
+  return (
+    <div id="command-panel-root" className="container w-full h-screen flex">
+      <div className="w-1/6"></div>
+      <div className="w-2/3 bg-gray-700 mt-36 h-fit p-4 rounded border-gray-600 border-4">
+        <input
+          ref={inputRef}
+          type="text"
+          className="w-full text-4xl bg-gray-800 p-8"
+          value={cmd}
+          onFocus={(e) => {
+            e.target.setSelectionRange(100, 100);
+          }}
+          onChange={(e) => {
+            if (e.target.value.length > 0) {
+              setCmd(e.target.value);
+            }
+          }}
+        />
+      </div>
+      <div className="w-1/6"></div>
+    </div>
+  );
+}
+
+export default CommandPanel;
